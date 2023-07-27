9d1672467e9df2f6580065b6bac7bf1cf241c04b
Alejandro Gonzalez <greeny090315@gmail.com>
2023-07-27 13:19:03 -0400

    new feature

---
+++ client/pokedex/src/components/TypeList/TypeList.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./TypeList.css";
+
+interface TypeListProps {
+  List: string[];
+  filteredList: string[];
+  setFilteredList: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const TypeList: React.FC<TypeListProps> = ({
+  List,
+  filteredList,
+  setFilteredList,
+}) => {
+  const handleClick = (type: string) => {
+    console.log(type);
+    setFilteredList([type]);
+    console.log(filteredList);
+  };
+
+  return (
+    <div className="container">
+      {List.map((type) => (
+        <button
+          key={List.indexOf(type)}
+          onClick={() => handleClick(type)}
+          className={'button ${type.toLowerCase()}'}
+        >
+          {type}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default TypeList;
